2347f50a9090f900b9233072891adf55f45a1778
sebas6612 <sebas6612@gmail.com>
2023-04-24 19:50:05 -0500

    improvements made

--- Close_API.py
+++ Close_API.py
@@ -6,7 +6,8 @@ By Sebastian Moncada Duque
 """
 
 import csv
-import datetime
+import json
+from closeio_api import Client
 
 def import_csv_data(file_name):
     """reads any CSV file within the project folder, just by passing the name 
@@ -16,21 +17,52 @@ def import_csv_data(file_name):
     titles = data.pop(0) #separate the headers and the data
     return titles, data
 
-def get_leads(leads_data):
-    """get the list of the companies within the data"""
+def get_leads_names_and_states(leads_data):
+    """get the list of the companies within the data and the US States"""
     companies_list = []
+    states = []
     for lead_row in leads_data:
         if lead_row[0].strip() not in companies_list:
             companies_list.append(lead_row[0].strip())
-    return companies_list
+        if lead_row[6] not in states and lead_row[6] != "":
+            states.append(lead_row[6])
+    return companies_list, states
 
-def group_contacts_by_lead(leads_data, leads_names):
+def group_contacts_by_lead(leads_data, leads_names, headers):
     """group all the contacts from the same lead"""
     contacts_grouped_by_leads = []
     for lead_name in leads_names:
-        leads_group = [lead for lead in leads_data if lead[0] == lead_name]
-        contacts_grouped_by_leads.append(leads_group)
+        lead_contacts = [lead for lead in leads_data if lead[0] == lead_name] #gets all the contacs where lead matches the current lead's name
+        founded = lead_contacts[0][4]
+        revenue = lead_contacts[0][5]
+        state = lead_contacts[0][6]
+        contacts_details = []
+        for contact in lead_contacts: #creates a list with all the contacts of an specific lead
+            if ";" in contact[2]: #if there is a ";" means there are 2 emails
+                emails = [{"type": "office", "email": contact[2].split(";")[0]}, {"type": "office", "email": contact[2].split(";")[1]}]
+            else:
+                emails = [{"type": "office", "email": contact[2]}]
+            if ";" in contact[3]: #if there is a ";" means there are 2 phone numbers
+                phones = [{"type": "office", "phone": contact[3].split(";")[0]}, {"type": "office", "phone": contact[3].split(";")[1]}]
+            else:
+                phones = [{"type": "office", "email": contact[3]}]
+            contacts_details.append({"name": contact[1], "emails": emails, "phones": phones})
+        contacts_grouped_by_leads.append({
+                            "name": lead_name,
+                            "contacts": contacts_details, 
+                            headers[4]: founded,
+                            headers[5]: revenue,
+                            "addresses": [{"state": state,"country":"US"}]
+                        })
+    print(json.dumps(contacts_grouped_by_leads[0], indent=4))
     return contacts_grouped_by_leads
+
+def post_leads(leads_grouped):
+    """Post the given list of leads to the Close's API """
+    api = Client('api_5Dsq0sNtw1TwyiRPuMjylP.2BJZs6G9AQBEh5XnppxDUt')
+    for lead in leads_grouped:
+        resp = api.post('lead', data=lead)
+        print(resp["name"], resp["id"])
 
 def clean_data(data):
     """cleans the data to better use"""
@@ -38,89 +70,177 @@ def clean_data(data):
         if row[2] == "":
             row[2] = "notprovided@verify.email" #put a default email when empty
         else:
-            row[2] = row[2].replace(",", ";").replace('"', '') # replace "," for ";" and delete the char " 
-        row[3].replace('"', '') #delete the char " 
+            row[2] = row[2].replace(",", ";").replace('"', '') # replace "," for ";" and delete the char "
+        row[3].replace('"', '') #delete the char "
         if row[4] == "":
-            row[4] = "01.01.1900" #put a default date in case of empty to not leave it blank 
+            row[4] = "01.01.1900" #put a default date in case of empty to not leave it blank
         if row[5] == "":
-            row[5] = "0" #put a default number in case of empty to not leave it blank 
+            row[5] = "0" #put a default number in case of empty to not leave it blank
         else:
             row[5] = row[5].replace("$", "").replace(",", "") #remove "$" and "," characters from revenue
     return data
 
-def export_data_csv(header, data):
-    """creates a CSV file with the headers and data passed"""
-    with open('MOCK_DATA_CLEANED.csv', mode='w', encoding="UTF-8", newline='') as output_file:
-        file_writer = csv.writer(output_file)
-        file_writer.writerow(header)
-        file_writer.writerows(data)
-
-def get_leads_founded_between_2_dates(data, start_date, finish_date):
+def get_leads_founded_between_2_dates(start_date, finish_date):
     """get the name of the leads that were founded between a given range"""
-    for row in data:
-        day, month, year = row[4].split(".")
-        row[4] = datetime.datetime(int(year), int(month), int(day)) #replace the date with a datetime object
+    api = Client('api_5Dsq0sNtw1TwyiRPuMjylP.2BJZs6G9AQBEh5XnppxDUt')
 
-    leads_within_the_range = []
-    for row in data:
-        if row[4] == "": #if there is no founded date ignore that row
-            continue
-        if row[4] >= start_date and row[4] <= finish_date and row[0] not in leads_within_the_range: #if the lead is within the range and is not yet on the list, add that lead
-            leads_within_the_range.append(row[0])
-    return leads_within_the_range #returns the list
+    params = {
+        "limit": None,
+        "query": {
+            "negate": False,
+            "queries": [
+                {
+                    "negate": False,
+                    "object_type": "lead",
+                    "type": "object_type"
+                },
+                {
+                    "negate": False,
+                    "queries": [
+                        {
+                            "negate": False,
+                            "queries": [
+                                {
+                                    "condition": {
+                                        "before": {
+                                            "type": "fixed_local_date",
+                                            "value": finish_date,
+                                            "which": "end"
+                                        },
+                                        "on_or_after": {
+                                            "type": "fixed_local_date",
+                                            "value": start_date,
+                                            "which": "start"
+                                        },
+                                        "type": "moment_range"
+                                    },
+                                    "field": {
+                                        "custom_field_id": "cf_mndnBH5FHQUxYghNx6UJrCuHkGFfRzpwNWVaLZiBb5y",
+                                        "type": "custom_field"
+                                    },
+                                    "negate": False,
+                                    "type": "field_condition"
+                                }
+                            ],
+                            "type": "and"
+                        }
+                    ],
+                    "type": "and"
+                }
+            ],
+            "type": "and"
+        },
+        "results_limit": None,
+        "sort": [],
+        "_fields": {
+            "lead": ["name"]
+        }
+    }
 
-def segment_leads_by_state(data):
+    resp = api.post("/data/search", params)
+
+    #print(json.dumps(resp, indent=4))
+    return [name["name"] for name in resp["data"]] #returns just the names of Leads
+
+def segment_leads_by_state(data, _states):
     """Segment Leads by US States and write a CSV file with the report of the revenue"""
-    leads_by_state = []
-    states = []
-    for row in data:
-        if row[5] == "" or row[5] == "0": #if there is no revenue data ignore that lead
-            continue
-        helper = [row[0], float(row[5]), row[6]] #group just the data required
-        if helper not in leads_by_state: #if not yet on the list, add that data
-            leads_by_state.append(helper)
-        if row[6] not in states and row[6] != "": #save all the states
-            states.append(row[6])
-    
     output = []
-    for state in states: #starts looping through each state
-        leads = 0
+    for state in _states:
+        api = Client('api_5Dsq0sNtw1TwyiRPuMjylP.2BJZs6G9AQBEh5XnppxDUt')
+        #filters to get the leads from a specific state and where the revenue is greater than 0
+        params = {
+            "limit": None,
+            "query": {
+                "negate": False,
+                "queries": [
+                    {
+                        "negate": False,
+                        "object_type": "lead",
+                        "type": "object_type"
+                    },
+                    {
+                        "negate": False,
+                        "queries": [
+                            {
+                                "negate": False,
+                                "related_object_type": "address",
+                                "related_query": {
+                                    "negate": False,
+                                    "queries": [
+                                        {
+                                            "condition": {
+                                                "mode": "full_words",
+                                                "type": "text",
+                                                "value": state
+                                            },
+                                            "field": {
+                                                "field_name": "state",
+                                                "object_type": "address",
+                                                "type": "regular_field"
+                                            },
+                                            "negate": False,
+                                            "type": "field_condition"
+                                        }
+                                    ],
+                                    "type": "and"
+                                },
+                                "this_object_type": "lead",
+                                "type": "has_related"
+                            },
+                            {
+                                "negate": False,
+                                "queries": [
+                                    {
+                                        "condition": {
+                                            "gt": 0,
+                                            "type": "number_range"
+                                        },
+                                        "field": {
+                                            "custom_field_id": "cf_5KjFJKfF6DMsTl8P6e45nkQsfnwxoe4LpKABIz198uG",
+                                            "type": "custom_field"
+                                        },
+                                        "negate": False,
+                                        "type": "field_condition"
+                                    }
+                                ],
+                                "type": "and"
+                            }
+                        ],
+                        "type": "and"
+                    }
+                ],
+                "type": "and"
+            },
+            "results_limit": None,
+            "sort": [],
+            "_fields": {
+                "lead": ["name", "custom"]
+            },
+            "_limit": 100
+        }
+        resp = api.post("/data/search", params)
         lead_most_revenue = ""
         revenue = 0
         total_revenue = 0
-        for row in leads_by_state:
-            if row[2] == state:
-                total_revenue += row[1] #accumulates all the revenues of that state
-                leads+=1 # +1 lead in that state
-                if row[1] > revenue:
-                    revenue = row[1] #save the highest revenue in that state
-                    lead_most_revenue = row[0]
-        output.append([state, leads, lead_most_revenue, total_revenue, total_revenue/leads]) #add the states's info to the list
-    print(output)
-
-    #export the date to a CSV file
+        for i, row in enumerate(resp["data"]):
+            if row["custom.cf_5KjFJKfF6DMsTl8P6e45nkQsfnwxoe4LpKABIz198uG"] > revenue: #if current lead is the highest revenue
+                revenue = row["custom.cf_5KjFJKfF6DMsTl8P6e45nkQsfnwxoe4LpKABIz198uG"]
+                lead_most_revenue = row["name"]
+            total_revenue += row["custom.cf_5KjFJKfF6DMsTl8P6e45nkQsfnwxoe4LpKABIz198uG"]
+        output.append([state, i+1, lead_most_revenue, total_revenue, total_revenue/(i+1)]) #add the states's info to the list
+    
+    #export the data to a CSV file
     with open('output.csv', mode='w', encoding="UTF-8", newline='') as output_file:
         titles = ["US State", "Total number of leads", "The lead with most revenue", "Total revenue", "Median revenue"]
         file_writer = csv.writer(output_file)
         file_writer.writerow(titles)
         file_writer.writerows(output)
-
-
+    
 if __name__ == "__main__":
     headers, data = import_csv_data('MOCK_DATA.csv')
     cleaned_data = clean_data(data)
-    export_data_csv(headers, cleaned_data)
-    # start date and final date to search the lead that were founded inside that range
-    leads_between_range = get_leads_founded_between_2_dates(
-        cleaned_data,
-        datetime.datetime(1963,3,29),
-        datetime.datetime(1970,3,29)
-    )
-    #print(leads_between_range)
-
-    #
-    segment_leads_by_state(cleaned_data)
-
-
-
-
+    leads_list, states = get_leads_names_and_states(cleaned_data)
+    leads = group_contacts_by_lead(cleaned_data, leads_list, headers)
+    post_leads(leads)
+    print(get_leads_founded_between_2_dates("1990-01-01", "2000-01-01"))
+    segment_leads_by_state(cleaned_data, states)
--- README.md
+++ README.md
@@ -6,11 +6,10 @@ Here is the part 2 explanation.
 
 1. First I started with a manual validation of the information to understand what information I had and to see errors in the data to correct.
 2. Because some of the rows had empty spaces I created a function called "clean_data" where it goes through the rows one by one and if it finds an empty mail it adds one by default "notprovided@verify.email", if it finds an empty foundation date it adds "01.01.1900" and if it finds an empty revenue it adds "0". All this to avoid problems when injecting the information through the API.
-3. To get the list of all the leads I go row by row checking the name of the company and adding them to a list and every time I find a company that I don't have yet on the list I add it.
-4. to obtain the states I go through the rows one by one and each state that is not yet in my list I add it, then I go through each row of the data again and I compare it with each of the states. For this process in a variable called "accumulator" I add all the revenue of all the leads of the state, in a variable called "counter" I add 1 to indicate another lead for that state, and in another variable I save the highest revenue, so at the end of the process I get all this information, I save it in a list and I can continue with the same process but with the next state.
+3. To get the list of all the leads and States, I go row by row checking the name of the company and adding them to a list and every time I find a company that I don't have yet on the list I add it, the same with the States list.
+4. To obtain the list of the Leads that were founded between 2 dates, I make a "Post" API call where the parameters filters that and the API responds with that list.
+5. In order to get the inform of the States revenue, first I make a "Post" API call where the parameters let me get the list of the Leads of that specific State and where the revenue is greater than "0". During this iteration process I have a variable called "accumulator" where I add all the revenue of all the leads of the state, and in another variable I save the highest revenue, so at the end of the process I get all this information, I save it in a list and I can continue with the same process but with the next state.
 
-### NOTE:
-I started creating everything from scratch but when reviewing I discovered that you already had a repository with sample scripts and one of them was to get the information from a CSV file and inject them using the API grouping the contacts by leads, so I decided to use it, but you will also find the functions that I developed that do the same, only they don't do the API injection
 
 ## B. How to run the script?
 
@@ -19,4 +18,3 @@ I started creating everything from scratch but when reviewing I discovered that 
 3. make sure to have the CSV file inside the folder, in my case named: "MOCK_DATA.csv"
 4. `pip install -r requirements.txt`
 5. `python Close_API.py`
-6. `python csv_to_cio.py -k [API.KEY] MOCK_DATA_CLEANED.csv` put your API key
